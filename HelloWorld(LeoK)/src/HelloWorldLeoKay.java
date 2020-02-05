037593780df0b91de61e02a53f37c16d18c6669b
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2020-02-05 12:06:55 -0500

    Initial commit

---
+++ HelloWorld(LeoK)/src/HelloWorldLeoKay.java
@@ -0,0 +1,11 @@
+
+public class HelloWorldLeoKay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String FirstWord = "Hello";
+		String SecondWord = " World!";
+		System.out.println(FirstWord + SecondWord);
+	}
+
+}
